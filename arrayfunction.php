12bab0e27941cb3c1b9f9eb563841d7b8b87b238
Jacki078 <118806054+Jacki078@users.noreply.github.com>
2022-11-22 10:36:11 +0100

    first commit

---
+++ arrayfunction.php
@@ -0,0 +1,23 @@
+<?php
+function getmittelwert($par1){
+$groese = count($par1);
+$arraydlauf = 0;
+$wert = 0;
+while ($groese != 0){
+$wert + $par1[$arraydlauf];
+$groese - 1;
+$arraydlauf + 1;
+}
+$test1[2,5,12,146,13156,12]
+echo getmittelwert($test1)
+
+}
+
+function haswert($par1, $par2){
+
+}
+
+function sortarray($par1){
+
+}
+?>
